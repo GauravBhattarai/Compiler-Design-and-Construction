6159af0818d531fe96a28a717600cda335f0b192
Varuagme <57834469+GauravBhattarai@users.noreply.github.com>
2021-12-20 16:13:14 +0545

    Add files via upload

---
+++ Recursive Descent Parsing/Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,s;
+	printf("enter any two numbers:");
+	scanf("%d,%d",&a,&b);
+	s=a+b;
+	printf("sum is %d",s);
+	return 0;
+	getch();
+}
